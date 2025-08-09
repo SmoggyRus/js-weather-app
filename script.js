91a8ce99d0d213c8254eac4c28e6f250688eccda
Ruslan <puhov.r6@gmail.com>
2025-08-10 02:47:05 +0500

    Add JavaScript

---
+++ script.js
@@ -0,0 +1,54 @@
+const API_KEY = "b6091a3cc8f61d6ed7add5113e3eb203";
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+
+async function checkWeather(city) {
+
+    try {
+        const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+
+        document.querySelector('.weather').style.display = 'block';
+        document.querySelector('.error').style.display = 'none';
+
+
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '℃';
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+
+        if (data.weather[0].main == 'Clouds') {
+            weatherIcon.src = 'images/clouds.png'
+        } else if (data.weather[0].main == 'Clear') {
+            weatherIcon.src = 'images/clear.png'
+        } else if (data.weather[0].main == 'Rain') {
+            weatherIcon.src = 'images/rain.png'
+        } else if (data.weather[0].main == 'Drizzle') {
+            weatherIcon.src = 'images/drizzle.png'
+        } else if (data.weather[0].main == 'Mist') {
+            weatherIcon.src = 'images/mist.png'
+        }
+
+    } catch (error) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    }
+
+}
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+})
+
+searchBox.addEventListener('keydown', () => {
+    if (event.key === 'Enter') {
+        checkWeather(searchBox.value);
+    }
+});
